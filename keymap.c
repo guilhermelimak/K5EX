b4286154188c4e86a6cf5d20133119a869aae2e2
Guilherme Lima <guilhermelimak@gmail.com>
2019-04-18 15:14:21 -0300

    refactor: Remove unused osx key definitions

--- keymap.c
+++ keymap.c
@@ -2,16 +2,7 @@
 #include "debug.h"
 #include "action_layer.h"
 #include "version.h"
-// #include "keymap_german.h"
-// #include "keymap_nordic.h"
-// #include "keymap_french.h"
-// #include "keymap_spanish.h"
-// #include "keymap_hungarian.h"
 
-#define KC_MAC_UNDO LGUI(KC_Z)
-#define KC_MAC_CUT LGUI(KC_X)
-#define KC_MAC_COPY LGUI(KC_C)
-#define KC_MAC_PASTE LGUI(KC_V)
 #define KC_PC_UNDO LCTL(KC_Z)
 #define KC_PC_CUT LCTL(KC_X)
 #define KC_PC_COPY LCTL(KC_C)
